5f8de60b29ea7552df8fd5f0da98183428684040
Elite007 <alijonoveldorbek78@gmail.com>
2025-01-05 15:26:48 +0900

    dff

---
+++ hhh.js
@@ -0,0 +1,10 @@
+// let bin = [21, 55, 76, 88, 2];
+
+// let binCHanged = bin.map(num => num.toString(2));
+// console.log(binCHanged);
+
+
+// let str = ["ELDORBEK", "APPLE", "ORANGE", "BOOK"];
+
+// let changed = str.map(num => num.toLowerCase());
+// console.log(changed);
